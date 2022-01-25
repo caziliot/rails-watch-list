f9568ff1f1358a27b5111be3255e37d490f047a3
Camilo Zamora <cazmera.music@gmail.com>
2022-01-25 20:11:34 +0100

    Corrected error from rot

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -17,7 +17,7 @@ class ListsController < ApplicationController
   def create
     @list = List.new(lists_params)
     @list.save
-    redirect_to lists_path
+    redirect_to root_path
   end
 
   private
